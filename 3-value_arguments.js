f49f486e5585c9e15674ff1d8baaef1870fa1d29
Jorgeeshun <georgeprinceakeshun23@gmail.com>
2025-07-13 14:10:21 +0000

    Renamed 3-value_argument.js

--- 3-value_arguments.js
+++
@@ -1,9 +0,0 @@
-const firstArgument = process.argv[2];
-
-// Check if the first argument is undefined (meaning no argument was passed by the user).
-if (firstArgument === undefined) {
-  console.log("No argument");
-} else {
-  // If an argument exists, print it.
-  console.log(firstArgument);
-}
